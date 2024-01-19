3bcedfd461514ee67ae2b701ebae2f418eed936a
urghosthost <132405002+urghosthost@users.noreply.github.com>
2024-01-19 09:55:32 -0500

    Add files via upload

--- js/mainjs.js
+++ js/mainjs.js
@@ -32,27 +32,32 @@ function formFunction() {
 }
 // Set/Create Cookies
 function setCookie(cookieName, cookieValue, expireDays) {
-    let date = new Date();
-    date.setTime(date.getTime() + (expireDays*24*60*60*1000));
-    const expirationDate = "expires="+date.toUTCString();
-    document.cookie = cookieName +"=" + cookieValue +";" + expirationDate + "; domain=axcelmarkapplications.site; secure;";
+    const d = new Date();
+    d.setTime(d.getTime() + (expireDays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
 }
 
 // Get Value of Cookie
 function getCookie(cookieName) {
-    const name = cookieName + "=";
-    const cookieDecoded = decodeURIComponent(document.cookie); //to be careful
-    const cookieArray = cookieDecoded.split('; ');
-    let res;
-    cookieArray .forEach(val => {
-        if (val.indexOf(name) === 0) res = val.substring(name.length);
-    })
-    return res;
+    let name = cookieName + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+     for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
 
 // Function for "Click to Apply Button"
 function applyButton(option) {
-    setCookie('username',option,30);
+    setCookie("option",option,30);
     window.location = 'application.html';
 }
 
